c4a2baf747339250a7961f65a3f23599e3246cec
tsund <tsund@qq.com>
2020-08-09 21:08:57 +0800

    update frp-remote-desktop

--- script/shell/xcall.sh
+++ script/shell/xcall.sh
@@ -7,10 +7,18 @@ magenta='\e[95m'
 cyan='\e[96m'
 none='\e[0m'
 
-java_home=$JAVA_HOME
 cluster_array=(hadoop1 hadoop2 hadoop3)
 
+# 1. 获取输入参数个数，如果没有参数，直接退出
+argcount=$#
+if ((argcount == 0)); then
+    echo -e ------------------- ${yellow}no args${none} -------------------
+    exit
+fi
+
+# 2. 循环执行命令
+command=$@
 for host in ${cluster_array[@]}; do
     echo -e ------------------- $yellow$host$none -------------------
-    ssh $host "$java_home/bin/jps"
+    ssh $host $command
 done
